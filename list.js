d4e87aae7450fe26e76c9dcc915a788e9c12ff89
duxiaodong <duxiaodong@darlin.me>
2017-04-20 22:58:59 +0800

    fix proxy_pass error

--- list.js
+++ list.js
@@ -1,9 +1,9 @@
 const list = [
-  ['lin', 'https://ljian3377.github.io/'],
-  ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip/'],
-  ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde/'],
-  ['ngx', 'https://doxiaodong.github.io/ngx/'],
-  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker/']
+  ['lin', 'https://ljian3377.github.io'],
+  ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip'],
+  ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde'],
+  ['ngx', 'https://doxiaodong.github.io/ngx'],
+  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker']
 ]
 
 const black = [
