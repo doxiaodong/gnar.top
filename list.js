8d5ba1c56865035070bb7d626e3dab5626f0ee95
doxiaodong <reduxiaodong@gmail.com>
2017-04-20 22:48:39 +0800

    Update list.js

--- list.js
+++ list.js
@@ -1,5 +1,5 @@
 const list = [
-  ['lin', 'https://ljian3377.github.io'],
+  ['lin', 'https://ljian3377.github.io/'],
   ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip/'],
   ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde/'],
   ['ngx', 'https://doxiaodong.github.io/ngx/'],
