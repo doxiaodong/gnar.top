4416188220afc160cb4a4672a4c9a9e0da0e0c60
duxiaodong <duxiaodong@darlin.me>
2017-04-28 10:54:14 +0800

    rm apple

--- list.js
+++ list.js
@@ -1,5 +1,4 @@
 const list = [
-  ['apple', 'https://www.apple.com'],
   ['lin', 'https://ljian3377.github.io'],
   ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip'],
   ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde'],
