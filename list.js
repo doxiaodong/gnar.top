038cfbd25a6a6302565ec6c5f3a15b1f2419b3dd
darlin <duxiaodong@darlin.me>
2017-08-09 11:29:37 +0800

    add ss-us03

--- list.js
+++ list.js
@@ -3,7 +3,8 @@ const list = [
   ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip'],
   ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde'],
   ['ngx', 'https://doxiaodong.github.io/ngx'],
-  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker']
+  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker'],
+  ['ss-us03', 'us03-21.ssv7.net:29287']
 ]
 
 const black = [
