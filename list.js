cf33c531393b67f786d1222cb1a0217b7cd6bbc1
doxiaodong <reduxiaodong@gmail.com>
2017-08-12 23:16:15 -0500

    remove ss

--- list.js
+++ list.js
@@ -3,8 +3,7 @@ const list = [
   ['ng2-clip', 'https://doxiaodong.github.io/ng2-clip'],
   ['ng2-simplemde', 'https://doxiaodong.github.io/ng2-simplemde'],
   ['ngx', 'https://doxiaodong.github.io/ngx'],
-  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker'],
-  ['ss-us03', 'http://us03-21.ssv7.net:29287']
+  ['range-datetimepicker', 'https://doxiaodong.github.io/range-datetimepicker']
 ]
 
 const black = [
